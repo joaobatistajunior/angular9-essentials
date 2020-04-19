24a466558552cf0d022f98dcc285efcec3f5c0a2
joaobjunior <joaobatista.junior@thomsonreuters.com>
2020-04-19 11:19:21 -0300

    Added create product method

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Router } from '@angular/router';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-create',
@@ -9,16 +10,25 @@ import { Router } from '@angular/router';
 })
 export class ProductCreateComponent implements OnInit {
 
+  product: Product = {
+    name: '',
+    price: null
+  }
+
   constructor(private productService: ProductService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   createProduct(): void {
-    this.productService.showMessage('Hello World')
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Hello World');
+    this.router.navigate(["/products"]);
+
+    });
   }
 
-  
+
   cancel(): void {
     this.router.navigate(["/products"]);
   }
